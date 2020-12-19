3842e783820201398cc9c6b1ddd3501f5e2694b4
Dmitry <konnerspz@mail.ru>
2020-12-19 17:57:05 +0300

    work on the bugs, task2_done

--- lesson_4/task_2.js
+++ lesson_4/task_2.js
@@ -1,33 +1,26 @@
-function countBasketPrice(cart) {
-    let total = 0;
-    for (const prop in cart) {
-        total += cart[prop].price * cart[prop].quantity
-    }
-    return total
-}
-
-
 let shoppingCart = {
-    mouse: {
-        price: 100,
-        quantity: 3 
-    },
-    board: {
-        price: 200,
-        quantity: 5 
-    },
-    monitor: {
-        price: 900,
-        quantity: 1 
-    },
-    countBasketPrice() {
-        let total = 0;
-        for (const prop in shoppingCart) {
-            total += this.prop.price * this.prop.quantity
+    products: [
+        {
+            title: 'mouse',
+            price: 100,
+            quantity: 3
+        },
+        {
+            title: 'board',
+            price: 200,
+            quantity: 5
+        },
+        {
+            title: 'monitor',
+            price: 900,
+            quantity: 1
         }
-        return total;
+    ],
+
+    countBasketPrice() {
+        return this.products.reduce((accumulator, currentValue) =>
+            accumulator + currentValue.price * currentValue.quantity, 0);
     }
 }
 
-// console.log(countBasketPrice(shoppingCart));
 console.log(shoppingCart.countBasketPrice());
