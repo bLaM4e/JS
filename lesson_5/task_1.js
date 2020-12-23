40c4412edd370472eb642c99e3e5d3a2993cf678
Dmitry <konnerspz@mail.ru>
2020-12-24 00:04:35 +0300

    task2_done

--- lesson_5/task_1.js
+++ lesson_5/task_1.js
@@ -3,15 +3,13 @@
 const settings = {
     rowCount: 8,
     colCount: 8,
-    column: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
-    figureFirstLine: 'П',
-    figureSecondLine: ['Л', 'К', 'С', 'К', 'Ф', 'С', 'К', 'Л']
+    rowName: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 };
 
 const render = {
     settings,
 
-    initFirstLastRow() {
+    init() {
         const trElem = document.createElement('tr');
         for(let numerateCol = 0; numerateCol <= this.settings.colCount + 1; numerateCol++) {
             const tdElem = document.createElement('td');
@@ -19,31 +17,40 @@ const render = {
             if (numerateCol !== 0 && numerateCol !== 9) tdElem.textContent = numerateCol; 
         }
         document.getElementById('chess').appendChild(trElem);
-    },
-
-    initFirstLine() {
-        const firstLine = document.createElement('tr');
-        for(let col = 0; col < this.settings.colCount; col++) {
-            // let cell = document.createElement('td');
-            // firstLine.appendChild(cell)
-            firstLine.insertAdjacentHTML('beforeend', `<td>${this.settings.figureFirstLine}</td>`)
-        }
-        document.getElementById('chess').appendChild(firstLine);
-    },
-
-    init() {
-        this.initFirstLastRow();
-
-        this.initFirstLine();
 
         for(let row = 0; row < this.settings.rowCount; row++) {
             const trEl = document.createElement('tr');
             document.getElementById('chess').appendChild(trEl);
-            trEl.insertAdjacentHTML('afterbegin', `<td>${this.settings.column[row]}</td>`);
+            trEl.insertAdjacentHTML('afterbegin', `<td>${this.settings.rowName[row]}</td>`);
 
             for(let col = 0; col < this.settings.colCount; col++) {
                 const tdEl = document.createElement('td');
                 trEl.appendChild(tdEl);
+
+                if(row === 1 || row === 6) tdEl.textContent = 'П';
+
+                if(row === 0 || row === 7) {
+                    switch(col) {
+                        case 0:
+                        case 7:
+                            tdEl.textContent = 'Л';
+                            break;
+                        case 1:
+                        case 6:
+                            tdEl.textContent = 'К';
+                            break;
+                        case 2:
+                        case 5:
+                            tdEl.textContent = 'С';
+                            break;
+                        case 3:
+                            tdEl.textContent = 'К';
+                            break;
+                        case 4:
+                            tdEl.textContent = 'Ф';
+                            break;
+                    }
+                }
 
                 if (row % 2 === 0) {
                     (col % 2 === 0) ? (tdEl.style.backgroundColor = 'black') : (tdEl.style.backgroundColor = 'white')
@@ -52,10 +59,10 @@ const render = {
                 }
             }
 
-            trEl.insertAdjacentHTML('beforeend', `<td>${this.settings.column[row]}</td>`);
+            trEl.insertAdjacentHTML('beforeend', `<td>${this.settings.rowName[row]}</td>`);
         }
         
-        this.initFirstLastRow();
+        document.getElementById('chess').appendChild(trElem.cloneNode(true));
     }
 };
 
